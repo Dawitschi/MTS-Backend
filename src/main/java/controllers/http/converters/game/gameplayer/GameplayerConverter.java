353ec214abaab4a014b7ac48d81df3060ba104cc
David <Dawitschi008@gmail.com>
2024-02-10 19:55:39 +0100

    Whole Project as of date

--- src/main/java/controllers/http/converters/game/gameplayer/GameplayerConverter.java
+++ src/main/java/controllers/http/converters/game/gameplayer/GameplayerConverter.java
@@ -8,7 +8,9 @@ import main.java.services.DBServices.PlayerService;
 import main.java.services.DBServices.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class GameplayerConverter implements Converter<GameplayerRepresentation, GamePlayer> {
 
     @Autowired
